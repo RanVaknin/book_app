4ca0c92386cd5cfc8d376024b93395a0fcde7912
RAN VAKNIN <ranvaknin0@gmail.com>
2019-11-12 13:31:57 -0800

    get em to rendeR

--- server.js
+++ server.js
@@ -4,26 +4,47 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors')
-const ejs = require('ejs');
 const superagent = require('superagent');
-
+const ejs = require('ejs')
 
 
 const PORT = process.env.PORT || 3000;
-app.set(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 app.set('view engine', 'ejs')
 
 
+// app.get('*', (req, res) => res.status(404).send('This route does not exist'));
 
-app.get('/', (req,res) => {   
+app.get('/', (req, res) => {
   res.render('pages/index')
 })
 
-// app.get('/books', (req,res) => {
+app.post('/searches', bookHandler)
 
-// })
+function bookHandler(req, res) {
+  let url = `https://www.googleapis.com/books/v1/volumes?q=`;
 
+  if (req.body.search[1] === 'Author') {
+    url += `inauthor:${req.body.search[0]}`
+  }
+  if (req.body.search[1] === 'Title') {
+    url +=`intitle:${req.body.search[0]}`
+  }
 
+  superagent.get(url)
+    .then(data => {
+      let array = [];
+			data.body.items.map(book => array.push(new Book(book.volumeInfo))
+			)
+			res.render('pages/searches/show', {displayData: array} )
+		})
+}
 
-app.listen(PORT , () =>{ console.log(`listening on port : ${PORT}`)});
+function Book(book) {
+  this.title = book.title;
+  this.authors = book.authors;
+  this.description = book.description;
+}
+
+app.listen(PORT, () => { console.log(`listening on port : ${PORT}`) });
