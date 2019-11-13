113816575897c3b4a231a6ae69b0e92e3c2f9c6a
RAN VAKNIN <ranvaknin0@gmail.com>
2019-11-13 13:45:02 -0800

    bookshelf

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const superagent = require('superagent');
-const ejs = require('ejs')
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL)
+client.connect();
+client.on('error', err => console.error(err));
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 app.set('view engine', 'ejs')
+
 
 
 // app.get('*', (req, res) => res.status(404).send('This route does not exist'));
@@ -19,6 +23,19 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
   res.render('pages/index')
 })
+
+app.get('/books/:id', (req, res ) => {
+  console.log('route is correct')
+
+  let SQL = 'SELECT * FROM books WHERE id = $1;';
+  let values = [req.params.id];
+  return client.query(SQL, values)
+    .then(data => {
+      return res.render('pages/books/show.ejs' , {banana: data.rows})
+    })
+    .catch( err => console.error(err));
+
+});
 
 app.post('/searches', bookHandler)
 
@@ -35,13 +52,18 @@ function bookHandler(req, res) {
   superagent.get(url)
     .then(data => {
       let array = [];
-			data.body.items.map(book => array.push(new Book(book.volumeInfo))
-			)
-			res.render('pages/searches/show', {displayData: array} )
-		})
+      data.body.items.map(book => array.push(new Book(book.volumeInfo))
+      )
+      res.render('pages/searches/show', {displayData: array} )
+    })
+    .catch(err => {
+      res.render('pages/error');
+    });
 }
 
+
 function Book(book) {
+  this.thumbnail = book.imageLinks.smallThumbnail
   this.title = book.title;
   this.authors = book.authors;
   this.description = book.description;
